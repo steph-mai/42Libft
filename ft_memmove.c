a76e4c70470ae71bfbfd53dd50be1872e92d9e30
Stephanie Maire <stmaire@z2r10p1.42lyon.fr>
2025-11-10 17:58:20 +0100

    add memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,47 +1,52 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: stmaire <stmaire@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/10 14:02:34 by stmaire           #+#    #+#             */
-/*   Updated: 2025/11/10 17:55:12 by stmaire          ###   ########lyon.fr   */
+/*   Created: 2025/11/10 15:13:58 by stmaire           #+#    #+#             */
+/*   Updated: 2025/11/10 17:47:22 by stmaire          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t			i;
 	unsigned char	*s1;
 	unsigned char	*s2;
+	size_t			i;
 
 	s1 = (unsigned char *)src;
 	s2 = (unsigned char *)dest;
-	i = 0;
 	if (dest == src)
 		return (dest);
-	while (i < n)
+	if (s2 < s1 || s2 >= s1 + n)
+		ft_memcpy(s2, s1, n);
+	else
 	{
-		s2[i] = s1[i];
-		i++;
+		while (n > 0)
+		{
+			n--;
+			s2[n] = s1[n];
+		}
 	}
 	return (dest);
 }
-
-/*#include <stdio.h>
+/*
 #include <string.h>
+#include <stdio.h>
 
 int		main(void)
 {
-	char src[] = "stephanie";
-	char dest1[100];
-	char dest2[100];
-	memcpy(dest1, src, ft_strlen(src) + 1);
-	printf("memcpy : %s, %s\n", src, dest1);
-	ft_memcpy(dest2, src, ft_strlen(src) + 1);
-	printf("ft_memcpy : %s, %s\n", src, dest2);
+	char	str1[10] = "stephanie";
+	char	str2[12] = "stephanie";
+
+	printf("str1 = %s\n", str1);
+	memmove(str1 + 2, str1, 7);
+	printf("memmove : new string = %s\n", str1);
+	ft_memmove(str2 + 2, str2, 7);
+    printf("ft_memmove : new string = %s\n", str2);
 	return (0);
 }*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: stmaire <stmaire@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 08:39:08 by stmaire           #+#    #+#             */
-/*   Updated: 2025/11/10 14:01:37 by stmaire          ###   ########lyon.fr   */
+/*   Updated: 2025/11/10 15:13:44 by stmaire          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,6 @@ size_t	ft_strlen(const char *s);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 
 #endif
