e6556e911cf02c6323931e3ad5d9058e2d37e0e9
Stephanie Maire <stmaire@z2r10p1.42lyon.fr>
2025-11-10 15:12:07 +0100

    ft_memcpy added

---
+++ ft_memcpy.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stmaire <stmaire@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/10 14:02:34 by stmaire           #+#    #+#             */
+/*   Updated: 2025/11/10 15:11:22 by stmaire          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t			i;
+	unsigned char	*s1;
+	unsigned char	*s2;
+
+	s1 = (unsigned char *)src;
+	s2 = (unsigned char *)dest;
+	i = 0;
+	if (dest == src)
+		return (dest);
+	while (i < n)
+	{
+		s2[i] = s1[i];
+		i++;
+	}
+	return (s2);
+}
+
+/*#include <stdio.h>
+#include <string.h>
+
+int		main(void)
+{
+	char src[] = "stephanie";
+	char dest1[100];
+	char dest2[100];
+	char dest3[100];
+	memcpy(dest1, src, 20);
+	printf("memcpy : %s, %s\n", src, dest1);
+	ft_memcpy(dest2, src, 20);
+	printf("ft_memcpy : %s, %s\n", src, dest2);
+	return (0);
+}*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: stmaire <stmaire@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 08:39:08 by stmaire           #+#    #+#             */
-/*   Updated: 2025/11/10 13:38:40 by stmaire          ###   ########lyon.fr   */
+/*   Updated: 2025/11/10 14:01:37 by stmaire          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,9 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
-void	*memset(void *s, int c, size_t n);
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 
 #endif
