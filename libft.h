c57437348b3580ed7276a39ea2c9f221d621fdd7
Stephanie Maire <stmaire@z2r11p4.42lyon.fr>
2025-11-12 16:59:27 +0100

    add toupper and tolower

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: stmaire <stmaire@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 08:39:08 by stmaire           #+#    #+#             */
-/*   Updated: 2025/11/12 16:15:57 by stmaire          ###   ########lyon.fr   */
+/*   Updated: 2025/11/12 16:25:56 by stmaire          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+int		toupper(int c);
+int		tolower(int c);
 
 #endif
