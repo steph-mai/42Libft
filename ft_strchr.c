cc06058a24bc80fbad4a49b803782c56e092d33a
Stephanie Maire <stmaire@z1r9p1.42lyon.fr>
2025-11-13 16:51:35 +0100

    add strchr tests OK

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,25 +6,25 @@
 /*   By: stmaire <stmaire@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 17:26:38 by stmaire           #+#    #+#             */
-/*   Updated: 2025/11/13 10:03:06 by stmaire          ###   ########lyon.fr   */
+/*   Updated: 2025/11/13 16:50:15 by stmaire          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strchr(const char *s, int c)
 {
-	int		i;
-	char	*str;
+	int				i;
+	unsigned char	*str;
 
-	str = (char *)s;
+	str = (unsigned char *)s;
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] == c)
-			return (&str[i]);
+		if (str[i] == (unsigned char)c)
+			return ((char *)&s[i]);
 		i++;
 	}
-	if (c == '\0')
-		return (&str[i]);
+	if ((unsigned char)c == '\0')
+		return ((char *)&s[i]);
 	return (0);
 }
 
