583666449e5c48e18693d1e36f7c1ee9546fda64
Stephanie Maire <stmaire@z1r10p1.42lyon.fr>
2025-11-19 18:03:38 +0100

    main deleted

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,11 +6,13 @@
 /*   By: stmaire <stmaire@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/19 13:12:17 by stmaire           #+#    #+#             */
-/*   Updated: 2025/11/19 17:05:00 by stmaire          ###   ########lyon.fr   */
+/*   Updated: 2025/11/19 17:35:49 by stmaire          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-t_list *ft_lstlast(t_list *lst)
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
 {
 	if (!lst)
 		return (NULL);
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: stmaire <stmaire@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 08:39:08 by stmaire           #+#    #+#             */
-/*   Updated: 2025/11/19 13:03:04 by stmaire          ###   ########lyon.fr   */
+/*   Updated: 2025/11/19 17:41:07 by stmaire          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,5 +63,6 @@ t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
 
 #endif
