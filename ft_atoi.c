4d2bc29417ec595b74627b8309f4560a44b4d8ee
steph-mai <steffi.maire@gmail.com>
2025-11-20 09:43:33 +0100

    Refactor ft_atoi to simplify overflow handling
    Removed overflow check function and simplified atoi logic.

--- ft_atoi.c
+++ ft_atoi.c
@@ -12,47 +12,31 @@
 
 #include "libft.h"
 
-static int	ft_check_overflow(long result, int digit, int negative)
-{
-	if (result > LONG_MAX / 10)
-	{
-		if (negative == 1)
-			return (0);
-		return (-1);
-	}
-	if ((result == LONG_MAX / 10 && digit > LONG_MAX % 10))
-	{
-		if (negative == 1)
-			return (0);
-		return (-1);
-	}
-	return (1);
-}
-
 int	ft_atoi(const char *nptr)
 {
 	int		i;
 	long	result;
 	int		negative;
-	int		check;
-
+	
 	i = 0;
 	result = 0;
 	negative = 0;
 	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == 32)
 		i++;
 	if (nptr[i] == '-' || nptr[i] == '+')
-		negative = (nptr[i++] == '-');
+	{
+		if (nptr[i] == '-')
+			negative = 1;
+		i++;
+	}
 	while (nptr[i] >= '0' && nptr[i] <= '9')
 	{
-		check = ft_check_overflow(result, nptr[i] - '0', negative);
-		if (check != 1)
-			return (check);
-		result = result * 10 + nptr[i++] - '0';
+		result = result * 10 + nptr[i] - '0';
+		i++;
 	}
 	if (negative == 1)
-		return (-result);
-	return (result);
+		return ((int)-result);
+	return ((int)result);
 }
 
 /*#include <stdlib.h>
