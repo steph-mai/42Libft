8f779dce7c19b81c8e272a9d04734d29fefbe02c
steph-mai <steffi.maire@gmail.com>
2025-11-20 19:01:16 +0100

    Return malloc(0) for zero size allocation

--- ft_calloc.c
+++ ft_calloc.c
@@ -16,7 +16,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void	*tab;
 
 	if (size != 0 && nmemb > SIZE_MAX / size)
-		return (NULL);
+		return (malloc(0));
 	tab = malloc(size * nmemb);
 	if (tab == NULL)
 		return (NULL);
